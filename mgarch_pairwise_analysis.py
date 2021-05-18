e477d6338f8f2cb81aa64c93c9cdb2c7dda0dc62
ananyaj3 <jhaveri.ananya@gmail.com>
2021-05-17 19:25:55 -0500

    readme updated again

--- mgarch_pairwise_analysis.py
+++ mgarch_pairwise_analysis.py
@@ -15,7 +15,7 @@ with open('final_pair_dict.pickle', 'rb') as handle:
 count = 0
 for i in data_dict.keys():
     if data_dict[i]['cov'][0][1] < 0 or data_dict[i]['cov'][1][0] < 0:
-        print("NEGATIVE COVARIANCE FOUND IN " + i)
+        print("NEGATIVE COVARIANCE FOUND IN " + i + "  ")
         count+=1
 
 print("total negative covariance count is " + str(count) + " of 600 total pairs")
